26293da6b1c92057fe5848879be30cef24f74c6f
bryonyoni <bryonyoni@gmail.com>
2019-09-25 10:42:02 +0300

    added progressbar for timing typing

--- app/src/main/java/com/color/dictatethenfirebaseidunnowhatever/MainActivity.java
+++ app/src/main/java/com/color/dictatethenfirebaseidunnowhatever/MainActivity.java
@@ -1,13 +1,17 @@
 package com.color.dictatethenfirebaseidunnowhatever;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.google.firebase.database.DatabaseReference;
@@ -18,8 +22,18 @@ import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
     @Bind(R.id.typeEditText) EditText typeEditText;
+    @Bind(R.id.enteredSentencesRecyclerView) RecyclerView enteredSentencesRecyclerView;
+    @Bind(R.id.progressBarTimer) ProgressBar progressBarTimer;
     private final String CONVERSATIONS = "Conversation";
+    private final int MAX_I = 7000;
+    private final int TIME_INTERVAL = 10;
     private String oldText = "";
+
+    private InitTask IT = null;
+    private int i = MAX_I;
+    private boolean hasPausedTimer = false;
+    private boolean cancelTimerEntirely = false;
+    private boolean hasTimerStarted = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,13 +63,137 @@ public class MainActivity extends AppCompatActivity {
                 }catch (Exception e){
                     e.printStackTrace();
                 }
-
             }
 
             @Override
             public void afterTextChanged(Editable editable) {
+                if (!typeEditText.getText().toString().equals("")){
+                    cancelTimerEntirely = true;
+                    progressBarTimer.setProgress(140);
+                    i = MAX_I;
 
+                    hasTimerStarted = false;
+                    startTimer();
+                }else if(typeEditText.getText().toString().equals("")){
+                    cancelTimerEntirely = true;
+                    progressBarTimer.setProgress(140);
+                    i = MAX_I;
+                }
             }
         });
+
+        if (!typeEditText.getText().toString().equals("") && hasTimerStarted){
+            startTimer();
+        }
+    }
+
+    private void startTimer(){
+        cancelTimerEntirely = false;
+        IT = new InitTask();
+        IT.execute();
+    }
+
+    protected class InitTask extends AsyncTask<Context, Integer, String> {
+        // -- gets called just before thread begins
+        @Override
+        protected void onPreExecute() {
+            hasTimerStarted = true;
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(Context... params) {
+            while (i > 0 && !hasPausedTimer && !cancelTimerEntirely) {
+                try {
+                    Thread.sleep(TIME_INTERVAL);
+                    i -= 50;
+                    publishProgress(i);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            return "COMPLETE!";
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+            if(!typeEditText.getText().toString().equals("")) {
+                progressBarTimer.incrementProgressBy(-1);
+            }else{
+                cancelTimerEntirely = true;
+                progressBarTimer.setProgress(140);
+                i = MAX_I;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            if(!hasPausedTimer && !cancelTimerEntirely){
+                resetTimer();
+                Log("Timer --- ", "Timer has finished");
+            }
+            cancelTimerEntirely = false;
+            hasTimerStarted = false;
+        }
+    }
+
+    private void resetTimer() {
+        cancelTimerEntirely = true;
+        new InitTask2().execute();
+    }
+
+    protected class InitTask2 extends AsyncTask<Context, Integer, String> {
+        // -- gets called just before thread begins
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(Context... params) {
+            int j = 0;
+            while (j <= 140) {
+                try {
+                    Thread.sleep(TIME_INTERVAL);
+                    j += 10;
+                    publishProgress(j);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            return "COMPLETE!";
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+            progressBarTimer.incrementProgressBy(10);
+
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            progressBarTimer.setProgress(140);
+            i = MAX_I;
+            hasTimerStarted = false;
+            setSentenceInRecyclerView();
+        }
+
+    }
+
+    private void setSentenceInRecyclerView() {
+
+    }
+
+    private void Log(String tag,String message){
+        try{
+            Log.d(tag,message);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
     }
 }
